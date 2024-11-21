0ffe03435cdd2f12d48035d675bd70616cf8e504
artichaudhary0 <chaudhary.aarti1998@gmail.com>
2024-11-21 09:54:50 +0530

    Image

--- app/src/main/java/com/example/calculator/Form.java
+++ app/src/main/java/com/example/calculator/Form.java
@@ -1,11 +1,15 @@
 package com.example.calculator;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -17,11 +21,13 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.regex.Pattern;
 
 public class Form extends AppCompatActivity {
-private     EditText nameField ;
- private    EditText emailField ;
- private    EditText phoneField ;
+    private EditText nameField ;
+    private EditText emailField ;
+    private EditText phoneField ;
     private EditText passwordField ;
     private Button submitBtn ;
+
+    private ImageButton imageButton;
 
 
     @Override
@@ -45,6 +51,7 @@ private     EditText nameField ;
              }
          });
          clearFunction();
+         imageClick();
     }
 
     private  boolean validateForm(String name,String email, String phone ,String password){
@@ -110,9 +117,24 @@ private     EditText nameField ;
         phoneField = findViewById(R.id.userPhoneNumber);
         passwordField = findViewById(R.id.userPassword);
         submitBtn = findViewById(R.id.submitButton);
+        imageButton = findViewById(R.id.imageBtn);
     }
 
+    private void  imageClick(){
+        ImageButton btn = findViewById(R.id.imageBtn);
 
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Image Button" , Toast.LENGTH_SHORT).show();
+                String url = "https://www.placement.com/learn/what-is-a-personal-brand";
+                Intent i = new Intent(Intent.ACTION_VIEW); // navigate
+
+                i.setData(Uri.parse(url)); // => Uri => url=string
+                startActivity(i);
+            }
+        });
+    }
 
     private void clearFunction(){
         Button clearBtn = findViewById(R.id.clearButton);
